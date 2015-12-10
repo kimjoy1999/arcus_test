2f16263de6f866210de5046684a8c7eb7e32679a
kimjoy1999 <kimjoy1999@naver.com>
2015-12-10 10:59:50 +0900

    arcus test java code

---
+++ src/test/java/com/navercorp/arcus/DBListTest.java
@@ -0,0 +1,23 @@
+// DBListTest.java
+package com.navercorp.arcus;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DBListTest {
+
+    DBList db = new DBList();
+
+    @Before
+    public void makeDB() {
+       // db.makeDB();
+    }
+
+    @Test
+    public void checkDB() {
+       // Assert.assertEquals("성공", db.checkDB());
+    }
+
+}
---
+++ src/test/java/com/navercorp/arcus/HelloArcusTest.java
@@ -0,0 +1,23 @@
+// HelloArcusTest.java
+package com.navercorp.arcus;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloArcusTest {
+
+    HelloArcus helloArcus = new HelloArcus("127.0.0.1:2181", "test");
+
+    @Before
+    public void sayHello() {
+        //helloArcus.sayHello();
+    }
+
+    @Test
+    public void listenHello() {
+        //Assert.assertEquals("성공", helloArcus.listenHello());
+    }
+
+}
